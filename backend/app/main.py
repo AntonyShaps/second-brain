668af7cbbb097f97aaed56118d58de9d0f3359bf
Anton Shapovalov <16188347+AntonyShaps@users.noreply.github.com>
2026-08-07 20:11:39 +0200

    fix: remove db init from main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.database import Base, engine
-from app.entries.models import EntryModel
-
 from app.entries.router import router as entries_router
 
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Second Brain API")
 
